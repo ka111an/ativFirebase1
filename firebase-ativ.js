1743dcfefd55cf88cd66de10ebb93e0c8e6bb07a
ka111an <126897702+ka111an@users.noreply.github.com>
2023-10-10 20:22:31 -0300

    Add files via upload

---
+++ firebase-ativ.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBv8VcI2MLZEVHWxJB9RyUCJHGmeYeAAdo",
+  authDomain: "kauanmendes1-cc9dd.firebaseapp.com",
+  projectId: "kauanmendes1-cc9dd",
+  storageBucket: "kauanmendes1-cc9dd.appspot.com",
+  messagingSenderId: "493805162961",
+  appId: "1:493805162961:web:36719eb342a34ca2d0d0f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
